f9924f2a75d0304a6314c1f2a507fe858fa8abd6
srpinfo7 <srpinfo7@gmail.com>
2025-06-11 22:06:08 +0530

    New Changes

--- src/test/java/GlobalProp/GlobalProp.java
+++ src/test/java/GlobalProp/GlobalProp.java
@@ -17,6 +17,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.AfterMethod;
@@ -34,81 +35,86 @@ public class GlobalProp {
 
 	public LoginPage login;
 	public WebDriver driver;
+
 	public WebDriver inializeDriver() throws IOException {
-	    Properties prop = new Properties();
-	    FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + "//src//test//java//GlobalProperties.properties");
-	    prop.load(fis);
-	    String browserName = prop.getProperty("browser").toLowerCase();
+		Properties prop = new Properties();
+		FileInputStream fis = new FileInputStream(
+				System.getProperty("user.dir") + "//src//test//java//GlobalProperties.properties");
+		prop.load(fis);
 
-	    switch (browserName) {
-	        case "chrome":
-	        case "chrome-headless":
-	            ChromeOptions chromeOptions = new ChromeOptions();
-	            chromeOptions.addArguments("--remote-allow-origins=*");
-	            chromeOptions.addArguments("--incognito");
-	            WebDriverManager.chromedriver().setup();
-	            if (browserName.contains("headless")) {
-	                chromeOptions.addArguments("--headless=new");
-	            }
-	            driver = new ChromeDriver(chromeOptions);
-	            break;
+		String browserName = System.getProperty("Browser") != null ? System.getProperty("Browser").toLowerCase()
+				: prop.getProperty("browser").toLowerCase();
+		// String browserName = prop.getProperty("browser").toLowerCase();
 
-	        case "firefox":
-	            // Force WebDriverManager to use a specific geckodriver version
-	        	FirefoxOptions options = new FirefoxOptions();
-	        	options.setBinary("C:\\Program Files\\Mozilla Firefox\\firefox.exe"); // adjust if installed elsewhere
-	        	driver = new FirefoxDriver(options);
-	            break;
+		switch (browserName) {
+		case "chrome":
+		case "chrome-headless":
+			ChromeOptions chromeOptions = new ChromeOptions();
+			chromeOptions.addArguments("--remote-allow-origins=*");
+			chromeOptions.addArguments("--incognito");
+			WebDriverManager.chromedriver().setup();
+			if (browserName.contains("headless")) {
+				chromeOptions.addArguments("--headless=new");
+			}
+			driver = new ChromeDriver(chromeOptions);
+			break;
 
-	        case "edge":
-	            WebDriverManager.edgedriver().setup();
-	            driver = new EdgeDriver();
-	            break;
+		case "firefox":
+			// Force WebDriverManager to use a specific geckodriver version
+			WebDriverManager.firefoxdriver().setup();
+			FirefoxOptions options = new FirefoxOptions();
+			options.addArguments("--incognito");
+			driver = new FirefoxDriver(options);
+			break;
 
-	        default:
-	            throw new IllegalArgumentException("Unsupported browser: " + browserName);
-	    }
+		case "edge":
+			WebDriverManager.edgedriver().setup();
+			EdgeOptions edgeOptions = new EdgeOptions();
+			edgeOptions.addArguments("--remote-allow-origins=*");
+			edgeOptions.addArguments("--incognito");
+			driver = new EdgeDriver(edgeOptions);
+			break;
 
-	    driver.manage().window().maximize();
-	    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-	    return driver;
+		default:
+			throw new IllegalArgumentException("Unsupported browser: " + browserName);
+		}
+
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		return driver;
 	}
 
-	
-	@BeforeMethod(alwaysRun=true)
-	public LoginPage lunchApp() throws IOException
-	{
-		driver=inializeDriver();
+	@BeforeMethod(alwaysRun = true)
+	public LoginPage lunchApp() throws IOException {
+		driver = inializeDriver();
 		login = new LoginPage(driver);
 		login.goTo();
 		return login;
 	}
-	
-	@AfterMethod(alwaysRun=true)
+
+	@AfterMethod(alwaysRun = true)
 	public void tearDown() {
-	    if (driver != null) {
-	        driver.close();
-	    }
+		if (driver != null) {
+			driver.close();
+		}
 	}
-	
-	public List<HashMap<String, String>> getJsonDataToMap(String path) throws IOException
-	{
-		String jsonContent=FileUtils.readFileToString(new File(path), StandardCharsets.UTF_8);
-		ObjectMapper mapper=new ObjectMapper();
-		List<HashMap<String,String>> data=mapper.readValue(jsonContent, new TypeReference<List<HashMap<String,String>>>(){
-			
-		});
+
+	public List<HashMap<String, String>> getJsonDataToMap(String path) throws IOException {
+		String jsonContent = FileUtils.readFileToString(new File(path), StandardCharsets.UTF_8);
+		ObjectMapper mapper = new ObjectMapper();
+		List<HashMap<String, String>> data = mapper.readValue(jsonContent,
+				new TypeReference<List<HashMap<String, String>>>() {
+
+				});
 		return data;
 	}
-	
-	
-	public String takeScreeShot(String testCaseName, WebDriver driver) throws IOException
-    {
-        TakesScreenshot screenShot = (TakesScreenshot) driver;
-        File source = screenShot.getScreenshotAs(OutputType.FILE);
-        String destinationFile = System.getProperty("user.dir")+"\\reports\\"+ testCaseName +".png";
-        FileUtils.copyFile(source, new File(destinationFile));
-        return destinationFile;
-    }
-	
+
+	public String takeScreeShot(String testCaseName, WebDriver driver) throws IOException {
+		TakesScreenshot screenShot = (TakesScreenshot) driver;
+		File source = screenShot.getScreenshotAs(OutputType.FILE);
+		String destinationFile = System.getProperty("user.dir") + "\\reports\\" + testCaseName + ".png";
+		FileUtils.copyFile(source, new File(destinationFile));
+		return destinationFile;
+	}
+
 }
